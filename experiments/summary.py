4dbaa8b259890d6a82726c36d8352103987a7098
nu1silva <nu1silva@gmail.com>
2025-05-22 22:37:49 +0200

    local usage with ollama

--- experiments/summary.py
+++ experiments/summary.py
@@ -6,18 +6,3 @@ from openai import OpenAI
 load_dotenv(override=True)
 api_key = os.getenv('OPENAI_API_KEY')
 
-# Check the key
-if not api_key:
-    print("No API key was found")
-elif not api_key.startswith("sk-proj-"):
-    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key")
-elif api_key.strip() != api_key:
-    print("An API key was found, but it looks like it might have space or tab characters at the start or end")
-else:
-    print("API key found and looks good so far!")
-
-openai = OpenAI()
-
-message = "Hello, GPT! This is my first ever message to you! Hi!"
-response = openai.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user", "content":message}])
-print(response.choices[0].message.content)
